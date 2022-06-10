513bd5acf2ba0e0d7acf008ec5651ff2bf492cb6
gfrancodsantos <gfrancodsantos@gmail.com>
2022-06-10 14:14:42 -0300

    1414

--- script.js
+++ script.js
@@ -13,56 +13,7 @@ function fixarMenuNoTopo() {
         nav.classList.remove("FixoNoTopo");
     }
 }
-/*
-var balls = document.querySelector('.balls')
-var quant = document.querySelectorAll('.slides .image')
-var atual = 0
-var imagem = document.getElementById('atual')
-var next = document.getElementById('next')
-var voltar = document.getElementById('voltar')
 
-for (let i = 2; i < quant.length; i++) {
-    var div = document.createElement('div')
-    div.id = i
-    balls.appendChild(div)
-}
-document.getElementById('0').classList.add('imgAtual')
-
-var pos = document.querySelectorAll('.balls div')
-
-for (let i = 2; i< pos.length; i++) {
-    pos[i].addEventListener('click', ()=> {
-        atual = pos[i].id
-        slide()
-    })
-}
-
-voltar.addEventListener('click',()=>{
-    atual--
-    slide()
-})
-
-next.addEventListener('click',()=>{
-    atual++
-    slide()
-})
-
-function slide() {
-    if (atual >= quant.length) {
-        atual = 0
-    }
-    else if (atual < 0) {
-        atual = quant.length - 1
-    }
-    document.querySelector('.imgAtual').classList.remove('.imgAtual')
-    imagem.style.marginLeft = -1000*atual+'px'
-    document.getElementById(atual).classList.add('imgAtual')
-}
-setInterval(()=>{
-    atual++
-    slide()
-},4000)
-*/
 const btnMobile = document.getElementById('btn-mobile');
 
 function toggleMenu(event){
